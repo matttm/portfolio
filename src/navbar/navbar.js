03ddce7c36e9a4483d7052dd94c249a46bd020b1
matttm <matttmaloney@gmail.com>
2021-02-15 15:49:52 -0500

    working on navbar and seperated styles

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './navbar.css';
 
 export default class Navbar  extends React.Component {
     render() {
         return (
-            <header>
-                <ul>
-                    <li><a id="home" href="#">Matt Maloney</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+            <header id="navbar">
+                <nav>
+                    <ul>
+                        <li><a id="home" href="#">Matt Maloney</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
             </header>
         );
     }
